64a6b4ebcaf9fe2d92995eb7dc9d225378d3df5b
Lakshay Mittal <Lakshaymittal709@gmail.com>
2026-06-20 19:11:08 +0530

    Completed Do While Loop

---
+++ 01_Loops/03_Do_Whie_Loop.cpp/03_Reverse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    int i = 10 ; 
+
+    do{
+        
+        cout<<i<<endl;
+
+        i = i - 2 ;
+        
+    }
+     while(i>=1);
+}
